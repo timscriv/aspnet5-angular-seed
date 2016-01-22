8bf57e9bc1bfb06a521e6568f1ae138b4438d20e
Tim Scrivner <tscrivner1@gmail.com>
2016-01-22 14:25:20 -0600

    Updated to aspnetcore instead of aspnet5

--- AngularSeedAspNet5/src/AngularSeedAspNet5/Controllers/HomeController.cs
+++ AngularSeedAspNet5/src/AngularSeedAspNet5/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
-namespace AngularSeedAspNet5.Controllers
+namespace AngularSeedAspNetCore.Controllers
 {
     public class HomeController : Controller
     {
